f27b6767a4ea6731c8d64459fea37fe0c928c5a5
Shahed Nasser <shahednasser@gmail.com>
2021-11-06 02:16:20 +0200

    changed port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors({ origin: '*' }));
 
